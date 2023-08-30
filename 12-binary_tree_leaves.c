0d687dda99380f603273d0a8864b6aebddb764cc
glackyy <othmanesticky@gmail.com>
2023-08-30 13:44:08 +0100

    adding binary tree leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Entry Function
+ * @tree: Pointer
+ * Return: number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+size_t leavs = 0;
+
+if (tree)
+{
+leavs += (!tree->left && !tree->right) : 0;
+leavs += binary_tree_leaves(tree->left);
+leavs += binary_tree_leaves(tree->right);
+}
+return (leavs);
+}
