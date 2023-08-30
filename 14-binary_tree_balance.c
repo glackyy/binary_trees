ec8046b36a8d791c1ebb8cf55ede71a5a5727f38
glackyy <othmanesticky@gmail.com>
2023-08-30 15:02:05 +0100

    fixing binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,8 +23,8 @@ if (tree)
 {
 	size_t lt = 0, rt = 0;
 
-	lt = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	rt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	lt = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	rt = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 	return ((lt > rt) ? lt : rt);
 }
 return (0);
