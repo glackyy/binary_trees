f3691747b809d319119263af98a2e92c8cff9f36
glackyy <othmanesticky@gmail.com>
2023-08-30 15:38:26 +0100

    adding  binary tree is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -47,5 +47,10 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+unsigned char is_leaf(const binary_tree_t *tree);
+size_t depth(const binary_tree_t *tree);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+int perfect_recursive(const binary_tree_t *tree,
+		size_t leaf_depth, size_t level);
 
 #endif
