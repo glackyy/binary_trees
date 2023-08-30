db6b9be5ea5d6ed6b9190cbccc6a8ebcc8a17b4f
glackyy <othmanesticky@gmail.com>
2023-08-30 10:38:18 +0100

    adding binary tree is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Entry Function
+ * @node: Pointer
+ * Return: node is a leaf 1, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node == NULL || node->left != NULL || node->right != NULL)
+	return (0);
+return (1);
+}
