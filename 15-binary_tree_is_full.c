63def453abf7a4a8915a9d5a4baa9ac777f156a5
glackyy <othmanesticky@gmail.com>
2023-08-30 15:14:36 +0100

    adding binary tree is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * full_recursive - Entry Function
+ * @tree: Pointer
+ * Return: 1 Otherwise 0
+ */
+int full_recursive(const binary_tree_t *tree)
+{
+if (tree)
+{
+	if ((tree->left != NULL && tree->right == NULL) ||
+	    (tree->left == NULL && tree->right != NULL) ||
+            full_recursive(tree->left) == 0 ||
+	    full_recursive(tree->right) == 0)
+	     return (0);
+}
+return (1);
+}
+
+/**
+ * binary_tree_is_full - Entry Function
+ * @tree: Pointer
+ * Return: 1 Otherwise 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (tree == NULL)
+	return (0);
+return (full_recursive(tree));
+}
